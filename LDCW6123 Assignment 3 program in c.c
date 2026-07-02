b5aaac64098f0a4138ea5de21cbdc16b6aa2bd1a
unknown <abdurahman3700@gmail.com>
2026-07-02 17:40:27 +0800

    Added addition more than zero condition to views and results calculation with display

--- LDCW6123 Assignment 3 program in c.c
+++ LDCW6123 Assignment 3 program in c.c
@@ -2,11 +2,11 @@
 int main()
 {
 int view,like,comment,share,stop;
-float engage;
-printf("----------------------Tiktok Engagement Rate Calculator-------------------\n");
+float engage,interact;
+printf("---------------------- Tiktok Engagement Rate Calculator -------------------\n");
 printf("The purpose of this calculator is to calculate the total amount of\n");
 printf("engagement on a specific video based on the statistics found.\n");
-printf("--------------------------------------------------------------------------\n");
+printf("----------------------------------------------------------------------------\n");
 do{
 printf("Please enter the total number of likes: ");
 scanf("%d", &like);
@@ -16,7 +16,7 @@ printf("Please enter the total number of shares: ");
 scanf("%d", &share);
 printf("Please enter the total number of views: ");
 scanf("%d", &view);
-if (view<0||like<0||comment<0||share<0){
+if (view<=0||like<0||comment<0||share<0){
 printf("You have entered an invalid value, please input again\n");
 }
 else {
@@ -24,6 +24,19 @@ stop=1;
 }
 }
 while (stop!=1);
+printf("---------------------------------- Results --------------------------------\n");
+interact=like+comment+share;
+printf("Total Interaction: %.0f\n", interact );
+engage=(interact/view)*100;
+printf("Engagement Rate: %.2f%% \n", engage);
+if(engage>=10)
+printf("Catagory:Viral");
+else if (engage<10 && engage>=5)
+printf("Catagory:High Engagement");
+else if (engage<5 && engage>=1)
+printf("Catagory:Average Engagement");
+else
+printf("Catagory:Low Engagement");
 return 0;
 }
 
