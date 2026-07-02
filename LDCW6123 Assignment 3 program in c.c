cd41b84cc978b82f229b682d844072fc07c28f44
unknown <abdurahman3700@gmail.com>
2026-07-02 15:43:54 +0800

    New file with variables, and prompting information

---
+++ LDCW6123 Assignment 3 program in c.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+int view,like,comment,share, confirm;
+float engage;
+printf("----------------------Tiktok Engagement Rate Calculator-------------------\n");
+printf("The purpose of this calculator is to calculate the total amount of\n");
+printf("engagement on a specific video based on the statistics found.\n");
+printf("--------------------------------------------------------------------------\n");
+printf("Please enter the total number of likes: ");
+scanf("%d", &view);
+printf("Please enter the total number of comments: ");
+scanf("%d", &view);
+printf("Please enter the total number of shares: ");
+scanf("%d", &view);
+printf("Please enter the total number of views: ");
+scanf("%d", &view);
+return 0;
+}
+
