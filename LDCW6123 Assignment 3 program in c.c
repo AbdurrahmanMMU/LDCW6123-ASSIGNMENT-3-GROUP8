d9a7a251465ef661b3c5b90a6a79941b54dde05d
unknown <abdurahman3700@gmail.com>
2026-07-02 18:03:29 +0800

    Added feature to restart the program or terminate

--- LDCW6123 Assignment 3 program in c.c
+++ LDCW6123 Assignment 3 program in c.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main()
 {
-int view,like,comment,share,stop;
+int view,like,comment,share,stop1,stop2;
 float engage,interact;
+char answer;
 printf("---------------------- Tiktok Engagement Rate Calculator -------------------\n");
 printf("The purpose of this calculator is to calculate the total amount of\n");
 printf("engagement on a specific video based on the statistics found.\n");
 printf("----------------------------------------------------------------------------\n");
+do{
 do{
 printf("Please enter the total number of likes: ");
 scanf("%d", &like);
@@ -20,10 +22,10 @@ if (view<=0||like<0||comment<0||share<0){
 printf("You have entered an invalid value, please input again\n");
 }
 else {
-stop=1;
+stop1=1;
 }
 }
-while (stop!=1);
+while (stop1!=1);
 printf("---------------------------------- Results --------------------------------\n");
 interact=like+comment+share;
 printf("Total Interaction: %.0f\n", interact );
@@ -37,6 +39,19 @@ else if (engage<5 && engage>=1)
 printf("Catagory:Average Engagement");
 else
 printf("Catagory:Low Engagement");
+
+printf("\nWould you like to stop the program?\nEnter Y to stop, enter N to retry\n");
+scanf(" %c", &answer);
+switch (answer){
+case 'y':
+stop2=1;
+break;
+case 'n':
+printf("Restarting\n");
+break;
+}
+}
+while (stop2!=1);
 return 0;
 }
 
